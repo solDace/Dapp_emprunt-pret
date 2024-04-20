5ceeb5e6b64e869502aa6182ddaf97d54fd94872
solDace <loic.sorroche@isen.yncrea.fr>
2024-04-20 17:12:26 -0400

    fin du projet

--- ihm/ihm.py
+++ ihm/ihm.py
@@ -8,11 +8,12 @@ class WalletManager:
     def __init__(self):
         self.w3 = self.__create_web3_instance()
         self.account = '0xe23B903Cd74F630fD87d504119682E3442f3D511'
-        self.account_private_key = ''
-        self.max_fee_per_gas = self.w3.to_wei('250', 'gwei')
+        self.account_private_key = 'ma clé privée que je ne vais pas vous donner'
+        self.max_fee_per_gas = self.w3.to_wei('250000000000000', 'wei')
+        self.gas_limit = 3000000
         self.max_priority_fee_per_gas = self.w3.eth.max_priority_fee
         self.chain_id = 11155111
-        self.contract_address = '0x3F2c73a8be1385867d22e80668A125Cbd738f9Ab'
+        self.contract_address = '0xe4fBeC2953aA37e70451e70FCbad56E2ff9b8196'
 
         with open('../ABI/ABI.json', 'r') as abi_file:
             self.contract_abi = json.load(abi_file)
@@ -21,11 +22,11 @@ class WalletManager:
     @staticmethod
     def __create_web3_instance():
         infura_api_key = 'd7673ea500d44f6db368563a06856b57'
-        infura_api_key_secret = ''
+        infura_api_key_secret = 'ma clé privée que je ne vais pas vous donner'
         data = f'{infura_api_key}:{infura_api_key_secret}'.encode('ascii')
         basic_auth_token = base64.b64encode(data).strip().decode('utf-8')
 
-        infura_sepolia_endpoint = f'https://sepolia.infura.io/v3/'
+        infura_sepolia_endpoint = f'https://sepolia.infura.io/v3/ma clée api infura que je ne vais pas vous donner'
 
         headers = dict(Authorization=f'Basic {basic_auth_token}')
         return web3.Web3(web3.HTTPProvider(infura_sepolia_endpoint, request_kwargs=dict(headers=headers)))
@@ -62,36 +63,51 @@ class WalletManager:
     def investir_fonds(self):
         duration = int(input("Entrez la durée en mois: "))
         amount = int(input("Entrez le montant à investir: "))
-        tx_hash = self.contract.functions.investirFonds(duration).transact({'from': self.account, 'value': amount})
+
+        tx = self.contract.functions.investirFonds(duration).build_transaction(
+            self.get_tansaction_dict(self.gas_limit, amount))
+        tx_hash = self.send_and_sign_tx(tx)
+
         receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
         print("Transaction réussie. Montant investi:", amount, "TX Hash:", tx_hash.hex())
 
     def emprunter_fonds(self):
         duration = int(input("Entrez la durée en mois: "))
         amount = int(input("Entrez le montant à emprunter: "))
-        tx_hash = self.contract.functions.emprunterFonds(amount, duration).transact(
-            {'from': self.account, 'value': amount})
+
+        tx = self.contract.functions.emprunterFonds(amount, duration).build_transaction(
+            self.get_tansaction_dict(self.gas_limit, 0))
+        # nonce = self.w3.eth.get_transaction_count(to_checksum_address(self.account))
+        # tx['nonce'] = nonce
+        tx_hash = self.send_and_sign_tx(tx)
+
         receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
         print("Transaction réussie. Montant emprunté:", amount, "TX Hash:", tx_hash.hex())
 
-    def rembourser_emprunt(self):
-        tx_hash = self.contract.functions.rembouserEmprunt().transact({'from': self.account, 'value': 0})
-        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
-        print("Transaction réussie. Montant remboursé:", receipt['gasUsed'], "TX Hash:", tx_hash.hex())
-
     def retour_investissement(self):
-        tx_hash = self.contract.functions.retourInvestissement().transact({'from': self.account, 'value': 0})
+        tx = self.contract.functions.retourInvestissement().build_transaction(
+            self.get_tansaction_dict(self.gas_limit, 0))
+        tx_hash = self.send_and_sign_tx(tx)
+
         receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
-        print("Transaction réussie. Montant retourné:", receipt['gasUsed'], "TX Hash:", tx_hash.hex())
+        print("Transaction envoyé.", "TX Hash:", tx_hash.hex())
+
+    def rembourser_emprunt(self):
+        amount = self.contract.functions.recupererMontantARembourser().call({'from': self.account})
+        tx = self.contract.functions.rembouserEmprunt().build_transaction(
+            self.get_tansaction_dict(self.gas_limit, amount))
+        tx_hash = self.send_and_sign_tx(tx)
+
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        print("Transaction envoyé.\nGas utilisé", receipt['gasUsed'], " Tx Hash:", tx_hash.hex())
 
     def add_fond_contract_balance(self):
         new_balance = int(input("Entrez le solde a ajouter contrat en wei: "))
-        gas_limit = 100000
 
         old_balance = self.contract.functions.getContractBalance().call()
         # Détermine le prix du gaz
         tx = self.contract.functions.addFondContractBalances(new_balance).build_transaction(
-            self.get_tansaction_dict(gas_limit, new_balance))
+            self.get_tansaction_dict(self.gas_limit, new_balance))
 
         tx_hash = self.send_and_sign_tx(tx)
         print("Transaction réussie. Nouveau solde du contrat:", old_balance + new_balance, "\nTX Hash:", tx_hash.hex())
@@ -99,15 +115,16 @@ class WalletManager:
     def send_and_sign_tx(self, tx):
         # Signe la transaction avec la clé privée du propriétaire
         signed_tx = self.w3.eth.account.sign_transaction(tx, self.account_private_key)
+
         # Envoie la transaction signée
         tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
         return tx_hash
 
-    def get_tansaction_dict(self, gas_limit, new_balance):
+    def get_tansaction_dict(self, gas_limit, amount):
         return {
             'chainId': self.chain_id,  # Mettez l'ID de la chaîne Ethereum correspondant ici
             'gas': gas_limit,
-            'value': self.w3.to_wei(new_balance, 'wei'),  # Montant d'Ether à envoyer vers le contrat
+            'value': self.w3.to_wei(amount, 'wei'),  # Montant d'Ether à envoyer vers le contrat
             'nonce': self.w3.eth.get_transaction_count(to_checksum_address(self.account)),
         }
 
@@ -118,24 +135,27 @@ def main():
     wm = WalletManager()
     while True:
         print("\n\n================================")
+        print("0. Ontenir ma balance")
         print("1. Obtenir la balance du contrat")
         print("2. Investir des fonds")
         print("3. Emprunter des fonds")
-        print("4. Rembourser un emprunt")
-        print("5. Retourner l'investissement")
+        print("4. Retourner l'investissement")
+        print("5. Rembourser un emprunt")
         print("6. Ajouter du solde au contrat")
         print("7. Quitter")
         choice = input("Entrez votre choix: ")
-        if choice == '1':
+        if choice == '0':
+            print("Votre balance est de", wm.w3.eth.get_balance(to_checksum_address(wm.account)), "wei")
+        elif choice == '1':
             wm.show_contract_balance()
         elif choice == '2':
             wm.investir_fonds()
         elif choice == '3':
             wm.emprunter_fonds()
         elif choice == '4':
-            wm.rembourser_emprunt()
-        elif choice == '5':
             wm.retour_investissement()
+        elif choice == '5':
+            wm.rembourser_emprunt()
         elif choice == '6':
             wm.add_fond_contract_balance()
         elif choice == '7':
